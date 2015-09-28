b71e784b77ba00c449b2b891d2d4e7d90ff3b690
gali7221 <gali7221@gmail.com>
2015-09-28 16:32:14 -0400

    added functionality for female or male appropriate paragraphs

--- js/app.js
+++ js/app.js
@@ -2,14 +2,28 @@ angular.module('myApp', [])
 
 .controller('madlibsController', function($scope){
 	$scope.template = [{
-		maleName: 'male name',
-        jTitle: 'job title',
-        tTask: 'tedious task',
-        dTask: 'dirty task',
-        celebrity: 'celebrity',
-        skill: 'useless skill',
-        ocelebrity: 'obnoxious celebrity',
-        hugeNum: 'huge number',
-        adjective: 'adjective'
+                name: 'name',
+                jTitle: 'job title',
+                tTask: 'tedious task',
+                dTask: 'dirty task',
+                celebrity: 'celebrity',
+                skill: 'useless skill',
+                ocelebrity: 'obnoxious celebrity',
+                hugeNum: 'huge number',
+                adjective: 'adjective'
 	}];
-})
+
+        $scope.newValue = function(value){
+                console.log(value);
+                if(value === "female"){
+                        $('.input-section').show();
+                        $('#female-text').show();
+                        $('#male-text').hide();
+                }
+                else if(value === "male") {
+                        $('.input-section').show();
+                        $('#male-text').show();
+                        $('#female-text').hide();
+                }
+        }
+});
