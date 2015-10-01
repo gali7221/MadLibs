cfd922bf23f07cdf7da2a2bb31ee9051320e9eb9
gali7221 <gali7221@gmail.com>
2015-10-01 15:05:31 -0400

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,29 +1,58 @@
-angular.module('myApp', [])
+angular.module('madLibs',['ngMessages'])
+  .controller('madlibsController', function($scope){
 
-.controller('madlibsController', function($scope){
-	$scope.template = [{
-                name: 'name',
-                jTitle: 'job title',
-                tTask: 'tedious task',
-                dTask: 'dirty task',
-                celebrity: 'celebrity',
-                skill: 'useless skill',
-                ocelebrity: 'obnoxious celebrity',
-                hugeNum: 'huge number',
-                adjective: 'adjective'
-	}];
+    $scope.template = [{
+      name: "name",
+      dTask: "dTask",
+      ocelebrity: "ocelebrity",
+      jobTitle: "jobTitle",
+      celebrity: "celebrity",
+      hugeNum: "hugeNum",
+      tTask: "tTask",
+      skill: "skill",
+      adjective: "adjective"  
+    }];
+    
 
-        $scope.newValue = function(value){
-                console.log(value);
-                if(value === "female"){
-                        $('.input-section').show();
-                        $('#female-text').show();
-                        $('#male-text').hide();
-                }
-                else if(value === "male") {
-                        $('.input-section').show();
-                        $('#male-text').show();
-                        $('#female-text').hide();
-                }
-        }
-});
+    $scope.showError = false;
+    $scope.showInput = true;
+    $scope.showMadLibs = true;
+
+
+
+    $scope.submit = function(){
+      if($scope.madLibForm.$valid ) {
+        console.log('The form is valid!');
+        $scope.showInput = false;
+        $scope.showError = false;
+        //console.log($scope.showInput);
+        $scope.showMadLibs = false;       
+      } 
+      else {
+        console.log('The form is NOT valid!');
+        $scope.showError = true;
+
+      }
+    };
+
+    $scope.resetForm = function(){
+      console.log('Reset!');
+      $scope.showMadLibs = true;
+      $scope.showInput = true;
+      
+      $scope.template.name = null;
+      $scope.template.dTask = null;
+      $scope.template.ocelebrity = null;
+      $scope.template.jobTitle = null;
+      $scope.template.celebrity= null;
+      $scope.template.hugeNum = null;
+      $scope.template.tTask = null;
+      $scope.template.skill = null;
+      $scope.template.adjective = null;
+      
+    };
+
+    $scope.display = false;
+    $scope.display1 = true;
+
+  });
